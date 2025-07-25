ae5ed15cb40d9745777917ff2c1647f7e92d65e8
Kaylane-Guarino <guarinokaah@gmail.com>
2025-07-25 17:00:49 -0300

    ajustes de erro no build

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { FaHeart } from "react-icons/fa";
-import styles from "./footer.module.scss";
+import styles from "./Footer.module.scss";
 
 export default function Footer() {
   return (
